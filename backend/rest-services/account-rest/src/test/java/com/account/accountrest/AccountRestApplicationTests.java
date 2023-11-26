2642d656d059ce78e4689635be4c2a9d0c8a85c7
cristian <cristiandavidfuentes@hotmail.com>
2023-11-26 03:56:10 -0500

    Feat: create base rest microservices

---
+++ backend/rest-services/account-rest/src/test/java/com/account/accountrest/AccountRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.accountrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
